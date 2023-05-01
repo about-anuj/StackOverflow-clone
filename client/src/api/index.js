c6408bacb028a49046746d973b80088d5dc9354c
anuj <63638503+about-anuj@users.noreply.github.com>
2023-05-01 16:03:51 +0530

    Update index.js
    added website link

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/'});
+const API = axios.create({ baseURL: 'https://legendary-boba-8a7495.netlify.app/'});
 
 API.interceptors.request.use((req) => {
     if (sessionStorage.getItem("Profile")) {
